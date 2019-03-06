2684384f81ab36b6d18340aed50c330dea2b03ef
overnick <overnic@httpd.cc>
2019-03-06 11:27:00 +0800

    fixbug crypt

--- src/Client/Crypt/Client.php
+++ src/Client/Crypt/Client.php
@@ -1,14 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: overnic
- * Date: 2019/3/6
- * Time: 10:04
- */
 namespace I3A\Api\Client\Crypt;
 
 use I3A\Api\Kernel\Abstracts\BaseClient;
-use I3A\Api\Kernel\Traits\AppCryptTrait;
 
 /**
  * 加密组件
@@ -18,5 +11,70 @@ use I3A\Api\Kernel\Traits\AppCryptTrait;
  */
 class Client extends BaseClient
 {
-    use AppCryptTrait;
+    /**
+     * 解密数据
+     *
+     * @param $data
+     * @param $key
+     * @return mixed
+     */
+    public function decrypt($data, $key = null)
+    {
+        return unserialize(openssl_decrypt(
+            hex2bin($data),
+            $this->getMode($this->getAesKey($key)),
+            $this->getAesKey($key),
+            $this->getAesIv($key)
+        ));
+    }
+
+    /**
+     * 加密数据
+     *
+     * @param $data
+     * @param $key
+     * @return string
+     */
+    public function crypt($data, $key = null)
+    {
+        return bin2hex(openssl_encrypt(
+            serialize($data),
+            $this->getMode($this->getAesKey($key)),
+            $this->getAesKey($key),
+            OPENSSL_RAW_DATA,
+            $this->getAesIv($key)
+        ));
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return string
+     */
+    protected function getMode($key)
+    {
+        return 'aes-'.(8 * strlen($key)).'-cbc';
+    }
+
+    /**
+     * 获取Aes加密Key
+     *
+     * @param null $key
+     * @return string
+     */
+    protected function getAesKey($key = null)
+    {
+        return md5($key ?? $this->app->config->get('access_key'));
+    }
+
+    /**
+     * 获取Aes加密盐值
+     *
+     * @param null $iv
+     * @return bool|string
+     */
+    protected function getAesIv($iv = null)
+    {
+        return substr(md5($iv ?? $this->app->config->get('access_key')),0,16);
+    }
 }
